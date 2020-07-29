ca24b5ca60f50f22fead84a7c08b2132cc36b92e
agungmandala <agung30mandala@gmail.com>
2020-07-29 18:28:13 +0700

    test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,22 +3,22 @@ import '../styles/index.scss';
 document.getElementById("tombol").addEventListener("click", function(event){
   event.preventDefault();
   console.log("haii");
-  
+
   const labelText = ["Create initial layout for homepage", "Discussions regarding workflow improvement", "Fixing icons with transparant background", "Create quotation for app redesign project"];
   const spanText = ["Approved","In Progress","In Review", "Waiting"];
- 
+
   const myList = document.getElementById("my-list");
-  
+
   // Create label element
   const label = document.createElement('label');
   // Add class
   label.className = 'list';
-  
+
   const spanLabel = document.createElement('span');
   spanLabel.className = 'status';
   var i = Math.floor(Math.random()*4);
   //console.log(i);
-  
+
   if(i===0){
 	  spanLabel.style.color = '#228B22';
 	  spanLabel.style.backgroundColor = '#bcdfc9';
@@ -32,24 +32,24 @@ document.getElementById("tombol").addEventListener("click", function(event){
 	  spanLabel.style.backgroundColor = '#cfcfcf';
   }
   spanLabel.innerHTML = spanText[i];
-  
+
   // Create text node and append to label
   label.innerHTML = `${labelText[Math.floor(Math.random()*4)]}`;
-  
+
   const input = document.createElement('input');
   input.type = 'checkbox';
-  
+
   label.appendChild(input);
-  
+
   const span = document.createElement('span');
-  span.className = 'checkmark'; 
+  span.className = 'checkmark';
   span.innerHTML = '';
-  
+
   label.appendChild(span);
   label.appendChild(spanLabel);
-  
+
   myList.appendChild(label);
-  
+
   //console.log(myList);
 
-});  
+});
